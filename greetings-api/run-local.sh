1170ae2b870abab213d37256a56bf65edf6dc4f4
Carlos Lopez <calopezb@redhat.com>
2022-02-02 16:15:32 +0100

    Fixed database setup for local development

--- greetings-api/run-local.sh
+++ greetings-api/run-local.sh
@@ -7,15 +7,16 @@ if [ "$( podman inspect -f '{{.State.Status}}' greeting-db )" != "running" ]; th
   podman run -d --name greeting-db \
     -e POSTGRES_USER=develop \
     -e POSTGRES_PASSWORD=develop \
-    -e POSTGRES_DB=greeting \
+    -e POSTGRES_DB=greeting-db \
     -p 5432:5432 \
     postgres:10.5
 
   # Wait until container is running
+  sleep 5
   until podman inspect -f '{{.State.Status}}' greeting-db | grep -m 1 "running"; do sleep 2 ; done
 
   podman cp ./src/main/resources/META-INF/sql/import-test.sql greeting-db:/docker-entrypoint-initdb.d/dump.sql
-  podman exec -u develop greeting-db psql postgres postgres -f docker-entrypoint-initdb.d/dump.sql
+  podman exec greeting-db /bin/bash -c 'psql greeting-db -U develop -W < /docker-entrypoint-initdb.d/dump.sql'
 
 fi
 
--- greetings-api/src/main/resources/META-INF/sql/import-test.sql
+++ greetings-api/src/main/resources/META-INF/sql/import-test.sql
@@ -1,7 +1,10 @@
+-- Import Script for testing and local dev
+
+CREATE TABLE IF NOT EXISTS  greeting ( id serial PRIMARY KEY, value VARCHAR ( 255 ));
+
 INSERT INTO greeting(id, value) VALUES (1, 'Hi there!');
 INSERT INTO greeting(id, value) VALUES (2, 'Ey buenas!');
 INSERT INTO greeting(id, value) VALUES (3, 'Morning!');
 INSERT INTO greeting(id, value) VALUES (4, 'Buenos diaaaas!!');
 
-
-ALTER SEQUENCE hibernate_sequence RESTART WITH 5;
+ALTER SEQUENCE IF EXISTS hibernate_sequence RESTART WITH 5;
